32fb40c7de58e89a83ec1366cd108cee59b84274
OneYourself21 <rajahaider08@hotmail.co.uk>
2026-09-01 21:58:31 +0100

    Changed timezone for polars reading from EST to ET

--- README.md
+++ README.md
@@ -3,5 +3,18 @@
 This takes databento csv format and into parquet. 
 ## Notes:
 - By default, polars uses zstd compression level 3
-- Data is stored in UTC
+- Data is stored in UTC but polars which has written it reads it as ET
 - Only for Nasdaq Futures (I don't know the format for options etc)
+- test.parquet is a duckdb version but neither duckdb nor polars read it as ET
+
+## Findings:
+
+- 2010-06-06->2012-11-15: maintenance break at 17:30
+- 2012-11-16->2015-09-18: maintenance break at 17:15
+- 2015-09-19->Now       : maintenance break at 17:00
+
+--- 
+
+### Disclaimer
+
+This was a quick small project to quickstart P6 duckdb/parquet adoption and not many tests on the data were conducted.
--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import polars as pl
+import duckdb
 
-if __name__ == '__main__':
+def write_db():
     q = pl.scan_csv("E-mini_Nasdaq-100_Futures/E-mini_Nasdaq-100_Futures.csv").select(
-        pl.col("ts_event").str.to_datetime(time_zone="EST"), "instrument_id", "symbol", "open", "high", "low", "close", "volume")
-
+        pl.col("ts_event").str.to_datetime(time_zone="America/New_York"), "instrument_id", "symbol", "open", "high", "low", "close",
+        "volume")
 
     df = q.collect()
-    df.write_parquet(file = "E-mini_Nasdaq-100_Futures.parquet")
+    df.write_parquet(file="E-mini_Nasdaq-100_Futures.parquet")
+
+    duckdb.sql("CREATE TABLE temp AS "
+               "SELECT * FROM df;")
+
+    duckdb.sql("COPY "
+               "(SELECT * FROM temp)"
+               "TO 'test.parquet' "
+               "(FORMAT parquet, COMPRESSION zstd, COMPRESSION_LEVEL 3);")
+
+    duckdb.sql("DROP TABLE temp;")
+
+
+
+def read_db():
+    q = pl.scan_parquet("E-mini_Nasdaq-100_Futures.parquet")
+    df = q.collect()
+    return df
+
+
+if __name__ == '__main__':
+    write_db()
